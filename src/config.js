1d9943c12eb7b3ea884d9c124f892b060a4264a9
Harry Chung <hc9825@gmail.com>
2019-08-20 13:23:41 -0700

    added jwt authorization

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/coach',
+  JWT_SECRET: process.env.JWT_SECRET || 'secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '60s',
 }
